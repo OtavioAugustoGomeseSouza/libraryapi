056a6e2d44e0bbea15742c6990944a3429240c54
Otávio Augusto Gomes e Souza <otaviosouza17@yahoo.com.br>
2025-07-30 14:17:57 -0300

    Projeto livraria

---
+++ libraryapi/src/test/java/cuso_java/libraryapi/Application.java
@@ -0,0 +1,13 @@
+package cuso_java.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
