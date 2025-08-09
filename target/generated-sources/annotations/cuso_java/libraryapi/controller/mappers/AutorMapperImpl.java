be07a20202e20a6313d87deb265bfcc2ad94b137
Otávio Augusto Gomes e Souza <otaviosouza17@yahoo.com.br>
2025-08-09 09:26:37 -0300

    -Adição do spring Secutity desabilitou csrf habilitei o http basic

--- target/generated-sources/annotations/cuso_java/libraryapi/controller/mappers/AutorMapperImpl.java
+++ target/generated-sources/annotations/cuso_java/libraryapi/controller/mappers/AutorMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-07T11:20:34-0300",
+    date = "2025-08-09T09:14:07-0300",
     comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.7 (Microsoft)"
 )
 @Component
